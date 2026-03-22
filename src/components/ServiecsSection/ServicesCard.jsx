a729d3a51010f00829d1697b0908e70f4d0269e3
themdshakibul <mdshakibul88240@gmail.com>
2026-03-22 21:55:10 +0600

    fixed

--- src/components/ServiecsSection/ServicesCard.jsx
+++ src/components/ServiecsSection/ServicesCard.jsx
@@ -5,7 +5,7 @@ const ServicesCard = ({ ServicesCard }) => {
   return (
     <>
       <div>
-        <div className="flex items-center p-10 space-y-5 card card-body text-center card-border shadow-2xl h-full w-full">
+        <div className="flex items-center p-10 space-y-5 card card-body text-center card-border shadow-2xl h-full w-full rounded-2xl">
           <img src={icon} alt="" className="w-16 flex-1" />
           <h2 className="text-2xl font-bold text-green-500">{title}</h2>
           <p className="text-xl font-semibold text-green-700">{description}</p>
