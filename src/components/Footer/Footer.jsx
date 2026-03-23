78dbe701b47d621c60a0165cffcebd1af803831a
themdshakibul <mdshakibul88240@gmail.com>
2026-03-23 19:14:44 +0600

    footer customije

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <>
       <div className="container mx-auto px-3">
-        <footer className="footer grid grid-cols-4 gap-10 text-base-content py-20">
+        <footer className="footer grid md:grid-cols-2 lg:grid-cols-4 gap-10 text-base-content py-20">
           <aside>
             <div className="flex items-center gap-2">
               <h2 className="bg-green-500 py-2 px-3 w-fit text-white rounded-md text-xl font-bold ">
@@ -12,31 +12,84 @@ const Footer = () => {
               </h2>
               <img src={navLogo} alt="Nav" />
             </div>
-            <p className="text-2xl font-medium">
+            <p className="text-2xl font-medium text-gray-500">
               Savor the artistry where every dish is a culinary masterpiece
             </p>
           </aside>
           <nav>
-            <h6 className="footer-title text-2xl font-semibold">Services</h6>
-            <a className="link link-hover text-xl font-medium">Branding</a>
-            <a className="link link-hover text-xl font-medium">Design</a>
-            <a className="link link-hover text-xl font-medium">Marketing</a>
-            <a className="link link-hover text-xl font-medium">Advertisement</a>
-          </nav>
-          <nav>
-            <h6 className="footer-title text-2xl font-semibold">Company</h6>
-            <a className="link link-hover text-xl font-medium">About us</a>
-            <a className="link link-hover text-xl font-medium">Contact</a>
-            <a className="link link-hover text-xl font-medium">Jobs</a>
-            <a className="link link-hover text-xl font-medium">Press kit</a>
-          </nav>
-          <nav>
-            <h6 className="footer-title text-2xl font-semibold">Legal</h6>
-            <a className="link link-hover text-xl font-medium">Terms of use</a>
-            <a className="link link-hover text-xl font-medium">
-              Privacy policy
+            <h6 className="text-2xl font-semibold text-black">Useful links</h6>
+            <a
+              href="#"
+              className="text-xl font-medium text-gray-500 hover:text-black"
+            >
+              About us
             </a>
-            <a className="link link-hover text-xl font-medium">Cookie policy</a>
+            <a
+              href="#"
+              className="text-xl font-medium text-gray-500 hover:text-black"
+            >
+              Events
+            </a>
+            <a
+              href="#"
+              className="text-xl font-medium text-gray-500 hover:text-black"
+            >
+              Blogs
+            </a>
+            <a
+              href="#"
+              className="text-xl font-medium text-gray-500 hover:text-black"
+            >
+              FAQ
+            </a>
+          </nav>
+          <nav>
+            <h6 className="text-2xl font-semibold text-black">Main Menu</h6>
+            <a
+              href="#"
+              className="text-xl font-medium text-gray-500 hover:text-black"
+            >
+              Home
+            </a>
+            <a
+              href="#"
+              className="text-xl font-medium text-gray-500 hover:text-black"
+            >
+              Offers
+            </a>
+            <a
+              href="#"
+              className="text-xl font-medium text-gray-500 hover:text-black"
+            >
+              Menus
+            </a>
+            <a
+              href="#"
+              className="text-xl font-medium text-gray-500 hover:text-black"
+            >
+              Reservation
+            </a>
+          </nav>
+          <nav>
+            <h6 className="text-2xl font-semibold text-black">Contact Us</h6>
+            <a
+              href="#"
+              className="text-xl font-medium text-gray-500 hover:text-black"
+            >
+              example@email.com
+            </a>
+            <a
+              href="#"
+              className="text-xl font-medium text-gray-500 hover:text-black"
+            >
+              +64 958 248 966
+            </a>
+            <a
+              href="#"
+              className="text-xl font-medium text-gray-500 hover:text-black"
+            >
+              Social media
+            </a>
           </nav>
         </footer>
       </div>
